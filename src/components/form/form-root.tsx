bf63e9b542b576185736c1bc61892898e6f268f5
FernandoJunior7 <fernando.junior@edu.unifil.br>
2024-10-28 15:23:57 -0300

    Fix: form styling

--- src/components/form/form-root.tsx
+++ src/components/form/form-root.tsx
@@ -1,6 +1,4 @@
 import type { HtmlHTMLAttributes, ReactNode } from 'react'
-import blobTop from '../../assets/Vector-2.svg'
-import blobBottom from '../../assets/Vector.svg'
 
 interface FormRootProps extends HtmlHTMLAttributes<HTMLFormElement> {
   children: ReactNode
@@ -8,24 +6,12 @@ interface FormRootProps extends HtmlHTMLAttributes<HTMLFormElement> {
 
 export function FormRoot({ children, onSubmit }: FormRootProps) {
   return (
-    <div className="min-h-screen grid place-content-center bg-gray-200 py-10">
-      <img
-        src={blobTop}
-        alt="Just a decorative blob at the top right"
-        className="fixed top-0 right-0"
-      />
-      <form
-        onSubmit={onSubmit}
-        className="flex flex-col gap-8 px-20 py-10 bg-gray-100 rounded-xl z-10"
-        noValidate
-      >
-        {children}
-      </form>
-      <img
-        src={blobBottom}
-        alt="Just a decorative blob at the bottom left"
-        className="fixed bottom-0 left-0"
-      />
-    </div>
+    <form
+      onSubmit={onSubmit}
+      className="flex flex-col gap-8 px-10 py-5 bg-gray-100 rounded-xl z-[1]"
+      noValidate
+    >
+      {children}
+    </form>
   )
 }
